2ae2bfdfc6db883cbd05739689a8b7691a659749
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2025-12-27 07:31:10 +0300

    fix: update lora folder path

--- comfyui_script_to_video_suite/s2v_nodes/s2v_auto_lora_node.py
+++ comfyui_script_to_video_suite/s2v_nodes/s2v_auto_lora_node.py
@@ -34,7 +34,7 @@ class AutoLoraLoader_S2V:
         Supports nested folders.
         """
         auto_map = {}
-        lora_root = folder_paths.get_folder_paths("loras")[0]
+        lora_root = folder_paths.get_folder_path("loras")
 
         for root, _, files in os.walk(lora_root):
             for filename in files:
